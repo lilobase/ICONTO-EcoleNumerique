ab2c9c1b2afb73cd622edc63bb08c1f229d20dc2
Christophe BEYER <cbeyer@cap-tic.fr>
2009-09-17 05:28:50 +0000

    Migration Copix 3

---
+++ project/modules/public/stable/iconito/magicmail/actiongroups/magicmail.actiongroup.php
@@ -0,0 +1,333 @@
+<?php
+/**
+ * Magicmail - ActionGroup
+ *
+ * @package	Iconito
+ * @subpackage  Magicmail
+ * @version     $Id: magicmail.actiongroup.php,v 1.10 2006-11-14 00:02:19 fmossmann Exp $
+ * @author      Frederic Mossmann <fmossmann@cap-tic.fr>
+ * @copyright   2006 CAP-TIC
+ * @link        http://www.cap-tic.fr
+ */
+
+require_once (COPIX_MODULE_PATH.'kernel/'.COPIX_CLASSES_DIR.'kernel.class.php');
+
+class ActionGroupMagicmail extends CopixActionGroup {
+
+
+   /**
+   * Configuration de MagicMail
+	 * 
+	 * @author Frederic Mossmann <fmossmann@cap-tic.fr>
+	 * @since 2006/08/09
+   */
+	function getMagicMail () {
+		$id = $this->vars["id"];
+		
+		if( Kernel::getLevel( "MOD_MAGICMAIL", $id ) < PROFILE_CCV_ADMIN ) {
+			return CopixActionGroup::process ('genericTools|Messages::getError',
+			array ('message'=>CopixI18N::get ('kernel|kernel.error.noRights'),
+			'back'=>CopixUrl::get ('||')));
+		}
+
+		$tpl = & new CopixTpl ();
+		$tpl->assign ('TITLE_PAGE', CopixI18N::get ('magicmail.message.title'));
+
+		$dao = CopixDAOFactory::create("magicmail|magicmail");
+		$magic_result = $dao->get($id);
+
+		$tplForm = & new CopixTpl ();
+		$tplForm->assign ('id', $id);
+		$tplForm->assign ('infos', $magic_result);
+		// $tplForm->assign ('magicmail_mail', CopixConfig::get ('magicmail|magicmail_mail'));
+		
+		if( isset( $this->vars["return"] ) )
+		$tplForm->assign ('return', $this->vars["return"]);
+		
+		CopixHTMLHeader::addCSSLink (_resource("styles/module_prefs.css"));
+
+		$tplForm->assign ('msg', array(
+			'type'  => 'ok',
+			'image_url' => 'img/iconito/prefs/smiley_black.png',
+			'image_alt' => 'Ok !',
+			'value' => CopixI18N::get ('prefs|prefs.msg.prefsrecorded')
+		) );
+		
+			
+		$result = $tplForm->fetch("login_form.tpl");
+		$tpl->assign ("MAIN", $result);
+
+		$nemu = array();
+		$returntoparent = Kernel::menuReturntoParent( "MOD_MAGICMAIL", $id );
+		if( $returntoparent ) $menu[] = $returntoparent;
+		$tpl->assign ('MENU', $menu );
+
+		return new CopixActionReturn (COPIX_AR_DISPLAY, $tpl);
+	}
+	
+	
+   /**
+   * Publication automatique par mail
+	 * 
+	 * @author Frederic Mossmann <fmossmann@cap-tic.fr>
+	 * @since 2006/07/28
+   */
+	function doMailPublish () {
+		
+				if( isset($this->vars["key"]) ) {
+					if( ereg( "^([0-9]{8}-[0-9]{6})-([a-zA-Z0-9]+)-([a-fA-F0-9]{32})$", $this->vars["key"], $regs ) ) {
+
+						$key   = $this->vars["key"];
+						$login = $regs[2];
+						
+						$dao = CopixDAOFactory::create("magicmail|magicmail");
+						$mymagicmail = $dao->getByLogin($login);
+						
+						$error = false;
+						if( sizeof($mymagicmail) ) {
+							$nodes = Kernel::getModParent( "MOD_MAGICMAIL", $mymagicmail[0]->magicmail_id );
+							if( sizeof($nodes) ) {
+							 $node_type = $nodes[0]->node_type;
+							 $node_id   = $nodes[0]->node_id;
+							} else $error = true;
+						} else $error = true;
+
+						if( !$error ) {
+							$file = @fopen( CopixConfig::get ('magicmail|magicmail_url').'data/'.$key.'/'.'info.txt', 'r' );
+							if ($file) {
+								$reply = '';
+								while (!feof($file)) {
+									$reply.= fgets($file, 4096);
+								} // while (!feof($file))
+								fclose($file);
+								
+								// <-- DEBUT TRAITEMENT -->
+								
+								//////////////////////////
+								
+								$blog_service = & CopixClassesFactory::Create ('blog|kernelblog');
+								$album_classes = & CopixClassesFactory::Create ('album|album');
+								$album_service = & CopixClassesFactory::Create ('album|kernelalbum');
+								
+								/*
+								$groupe_service = & CopixClassesFactory::Create ('groupe|groupeservice');
+								$blog = $groupe_service->getGroupeBlog( $grp );
+								$album = $groupe_service->getGroupeAlbum( $grp );
+								*/
+								
+								$blog  = null;
+								$album = null;
+								
+								$mods = Kernel::getModEnabled( $node_type, $node_id );
+								foreach( $mods AS $mod ) {
+									switch( $mod->module_type ) {
+										case 'MOD_ALBUM':
+											$album = $mod->module_id;
+											break;
+										case 'MOD_BLOG':
+											$blog = $mod->module_id;
+											break;
+									}
+								}
+								
+								/*
+								if( $blog == null ) {
+									echo "-ERR No Blog\n";
+									return new CopixActionReturn (COPIX_AR_NONE, 0);
+								}
+								if( $album == null ) {
+									echo "-ERR No Album\n";
+									return new CopixActionReturn (COPIX_AR_NONE, 0);
+								}
+								*/
+								
+								// echo "<br \>Blog: ".$blog." - Album: ".$album."<br \>\n";
+
+							//////////////////////////
+							
+									$blog_article = array();
+									$album_photo  = array();
+									$audio_files  = array();
+									
+									while (ereg ("^(SUBJECT|IMAGE|AUDIO|VIDEO-FLV|BODY) ?([^\n]+)\n(.*)$", $reply, $regs)) {
+										// echo "<li>".$regs[0]."<br/>\n<li>".$regs[1]."<br/>\n<li>".$regs[2]."<br/>\n<li>".$regs[3];
+										switch ($regs[1]) {
+											case 'SUBJECT':
+												$blog_article['title'] = $regs[2];
+												break;
+											case 'IMAGE':
+												if( ereg ("^([0-9a-fA-F]+) ([0-9]+) (.*)$", $regs[2], $image_data) ) {
+												
+													$photo_file = fopen( CopixConfig::get ('magicmail|magicmail_url').'data/'.$key.'/'.$image_data[1], 'r' );
+													$photo_data = '';
+													if ($file) {
+														while (!feof($photo_file)) {
+															$photo_data.= fgets($photo_file, 4096);
+														} // while (!feof($file))
+														fclose($photo_file);
+													}
+													
+													$album_photos[] = array(
+														'file' => $image_data[3],
+														'title' => $image_data[3],
+														'data' => $photo_data
+													);
+												}
+												break;
+											case 'AUDIO':
+												if( ereg ("^([0-9a-fA-F]+) ([0-9]+) (.*)$", $regs[2], $audio_data) ) {
+												
+													$audio_files[] = array(
+														'file' => CopixConfig::get ('magicmail|magicmail_url').'data/'.$key.'/'.$audio_data[1],
+														'title' => $audio_data[3]
+													);
+												}
+												break;
+											case 'VIDEO-FLV':
+												if( ereg ("^([0-9a-fA-F]+) ([0-9]+) (.*)$", $regs[2], $video_flv_data) ) {
+												
+													$video_flv_files[] = array(
+														'file' => CopixConfig::get ('magicmail|magicmail_url').'data/'.$key.'/'.$video_flv_data[1],
+														'title' => $video_flv_data[3]
+													);
+												}
+												break;
+											case 'BODY':
+												$blog_article['body'] = $regs[3];
+												break;
+										}
+										$reply = $regs[3];
+									}
+									
+									//////////////////////////
+									$images = array();
+									
+									if( $album != null ) {
+									
+										foreach( $album_photos AS $album_photo ) {
+											$album_photo_retour = $album_service->publish( $album, $album_photo );
+											$images[] = $album_photo_retour;
+										}
+									}
+									
+									if( $blog != null ) {
+										if( ! $blog_article['title'] || trim($blog_article['title'])=='' ) {
+											$date = date('d/m/Y \à H\hi');
+											$blog_article['title'] = 'Article du '.$date;
+										}
+										
+										foreach( $audio_files AS $audio_file ) {
+											$blog_article['body'] .= "\n".'[['.$audio_file['file']."|mp3]]\n";
+										}
+										
+										foreach( $video_flv_files AS $video_flv_file ) {
+											$blog_article['body'] .= "\n".'[['.$video_flv_file['file'].".flv|flv]]\n";
+										}
+										
+										foreach( $images AS $image ) {
+											$blog_article['body'] .= "\n".'[((static/album/'.$image['album_id'].'_'.$image['album_key'].'/'.
+											$image['photo_id'].'_'.$image['photo_key'].'_240.'.$image['photo_ext'].'|'.
+											$image['title'].'|))|static/album/'.$image['album_id'].'_'.$image['album_key'].'/'.
+											$image['photo_id'].'_'.$image['photo_key'].'.'.$image['photo_ext'].']'."\n";
+										}
+
+										$blog_retour = $blog_service->publish( $blog, $blog_article );
+										print_r( $blog_retour );
+									}
+									// <-- FIN TRAITEMENT -->
+	
+								echo "+OK\n";
+								// print_r( $images );
+							
+								// echo htmlentities($reply);
+							} // if ($file)
+							else echo "-ERR No info (file not found)\n";
+						} // if( $key==$source['key'] )
+						else echo "-ERR Bad key\n";
+					} // if( ereg( "^([0-9]{8}-[0-9]{4})-([a-zA-Z0-9]+)-([a-fA-F0-9]{32})$") )
+					else echo "-ERR Bad key (ereg not match)\n";
+				} // if( isset($this->vars["key"]) )
+				else echo "-ERR Bad key (not set)\n";
+	
+		return new CopixActionReturn (COPIX_AR_NONE, 0);
+	}
+
+	function doCreateMail() {
+		$id = $this->vars["id"];
+
+		$dao = CopixDAOFactory::create("magicmail|magicmail");
+		$mymagicmail = $dao->get($id);
+
+		// $newlogin = substr( md5(microtime()), 0, 10 );
+
+		$url = CopixConfig::get ('magicmail|magicmail_url');
+		$url.= 'register.php?action=add&url='.urlencode(CopixUrl::get()).'&id='.urlencode($id);
+		if( $mymagicmail && trim($mymagicmail->magicmail_login.'@'.$mymagicmail->magicmail_domain)!='' )
+			$url.= '&login='.urlencode(trim($mymagicmail->magicmail_login));
+		
+		$file = @fopen( $url, 'r' );
+		if ($file) {
+			$reply = '';
+			while (!feof($file)) {
+				$reply.= fgets($file, 4096);
+			} // while (!feof($file))
+			fclose($file);
+
+			$return = '';
+			if( ereg ("^\+OK (.+)@(.+)$", $reply, $data) ) {
+			
+				if( ! $mymagicmail ) {
+					$mymagicmail = CopixDAOFactory::createRecord("magicmail|magicmail");
+					$mymagicmail->magicmail_id    = $id;
+					$mymagicmail->magicmail_login = $data[1];
+					$mymagicmail->magicmail_domain = $data[2];
+					$dao->insert( $mymagicmail );
+			} else {
+//					echo( '<pre>'.print_r($mymagicmail,true).'</pre>');
+					$mymagicmail->magicmail_login = $data[1];
+					$mymagicmail->magicmail_domain = $data[2];
+//					echo( '<pre>'.print_r($mymagicmail,true).'</pre>');
+					$dao->update( $mymagicmail );
+//					echo( '<pre>'.print_r($mymagicmail,true).'</pre>');
+//					die('Gloups!');
+				}
+				
+				$return = 'ok';
+			} else {
+				$return = 'error';
+			}
+			
+		}
+
+		return new CopixActionReturn (COPIX_AR_REDIRECT, CopixUrl::get ('magicmail|default|go', array('id'=>$this->vars["id"],'return'=>$return) ) );
+	}
+
+	function doDeleteMail() {
+		$id = $this->vars["id"];
+		
+		$dao = CopixDAOFactory::create("magicmail|magicmail");
+		$mymagicmail = $dao->get($id);
+		
+		$url = CopixConfig::get ('magicmail|magicmail_url');
+		$url.= 'register.php?action=del&url='.urlencode(CopixUrl::get()).'&id='.urlencode($id);
+		if( $mymagicmail && trim($mymagicmail->magicmail_login)!='' )
+			$url.= '&login='.urlencode(trim($mymagicmail->magicmail_login));
+		
+		$file = @fopen( $url, 'r' );
+		if ($file) {
+			$reply = '';
+			while (!feof($file)) {
+				$reply.= fgets($file, 4096);
+			} // while (!feof($file))
+			fclose($file);
+		}
+
+		$mymagicmail->magicmail_login = '';
+		$mymagicmail->magicmail_domain = '';
+		$dao->update( $mymagicmail );
+
+		return new CopixActionReturn (COPIX_AR_REDIRECT, CopixUrl::get ('magicmail|default|go', array('id'=>$id) ) );
+	}
+	
+}
+?>
---
+++ project/modules/public/stable/iconito/sso/actiongroups/sso.actiongroup.php
@@ -0,0 +1,269 @@
+<?php
+/**
+ * Sso - ActionGroup
+ *
+ * @package	Iconito
+ * @subpackage  Sso
+ * @version     $Id: sso.actiongroup.php,v 1.6 2007-12-21 17:35:39 fmossmann Exp $
+ * @author      Frederic Mossmann <fmossmann@cap-tic.fr>
+ * @copyright   2006 CAP-TIC
+ * @link        http://www.cap-tic.fr
+ */
+
+require_once (COPIX_MODULE_PATH.'kernel/'.COPIX_CLASSES_DIR.'kernel.class.php');
+
+class ActionGroupSso extends CopixActionGroup {
+
+   function getSso () {
+      
+      $id = $this->getRequest('id', null);
+      
+   		if (!$id) {
+			// Récupération des infos de l'utilisateur.
+			$userInfo = Kernel::getUserInfo();
+			// Création des modules inexistants.
+			Kernel::createMissingModules( $userInfo["type"], $userInfo["id"] );
+			// Liste des modules activés.
+			$modsList = Kernel::getModEnabled( $userInfo["type"], $userInfo["id"] );
+			foreach( $modsList AS $modInfo ) {
+				if( $modInfo->module_type == "MOD_SSO" && $modInfo->module_id) {
+					$urlReturn = CopixUrl::get ('sso||getSso', array('id'=>$modInfo->module_id));
+					return new CopixActionReturn (COPIX_AR_REDIRECT, $urlReturn);	
+				}
+			}
+		}
+		
+
+    if (!$id)
+			return CopixActionGroup::process ('genericTools|Messages::getError', array ('message'=>'Problème', 'back'=>CopixUrl::get('||')));  
+        
+   
+		$tpl = & new CopixTpl ();
+
+		$tpl->assign ('TITLE_PAGE', CopixI18N::get ('sso.title.servext'));
+
+		
+
+		$dao = CopixDAOFactory::create("sso|sso_auth");
+		// $all = $dao->findAll();
+		$all = $dao->findBySso($id);
+		
+		$tpl->assign ('MAIN', CopixZone::process ('sso|SsoAuthList', array('list'=>$all, 'id'=>$this->vars["id"])) );
+		
+
+		return new CopixActionReturn (COPIX_AR_DISPLAY, $tpl);
+
+	}
+	
+	function doSso () {
+    
+    $criticErrors = array();
+    
+		$dao = CopixDAOFactory::create("sso|sso_auth");
+		$sso = $dao->get( $this->vars["id"] );
+		
+    if (!$sso)
+      $criticErrors[] = CopixI18N::get ('sso.error.nosso');
+    $mondroit = Kernel::getLevel( "MOD_SSO", $sso->sso_auth_id_sso);
+    if ($mondroit < PROFILE_CCV_ADMIN)
+      $criticErrors[] = CopixI18N::get ('sso.error.norights');
+
+		$url = $sso->sso_auth_url.'?mode=challenge';
+		$url.= '&login_ico='.urlencode($sso->sso_auth_login_local);
+		$url.= '&login_distant='.urlencode($sso->sso_auth_login_distant);
+		$file = fopen( $url, 'r' );
+		if ($file && !$criticErrors) {
+			$challenge = '';
+			while (!feof($file)) {
+				$challenge .= fread($file, 1024);
+			}
+			fclose ($file);
+			if ($challenge!='') {
+				$challenge_crypt = md5($challenge.$sso->sso_auth_secret_key);
+				$url = $sso->sso_auth_url.'?mode=login';
+				$url.= '&login_ico='.urlencode($sso->sso_auth_login_local);
+				$url.= '&login_distant='.urlencode($sso->sso_auth_login_distant);
+				$url.= '&key='.urlencode($challenge_crypt);
+				return new CopixActionReturn (COPIX_AR_REDIRECT, $url );
+			} else {
+        $criticErrors[] = CopixI18N::get ('sso.error.pbacces');
+      }
+		//	Kernel::MyDebug ($challenge);
+			
+		}
+
+    if ($criticErrors)
+			return CopixActionGroup::process ('genericTools|Messages::getError', array ('message'=>implode('<br/>',$criticErrors), 'back'=>CopixUrl::get('sso||')));  
+
+		//return new CopixActionReturn (COPIX_AR_REDIRECT, $url );
+
+	}
+  
+	/**
+	 * Affichage du formulaire d'ajout d'un service SSO
+	 * 
+	 * @author Christophe Beyer <cbeyer@cap-tic.fr>
+	 * @since 2006/11/14
+	 * @param integer $sso Id de la collection SSO
+	 * @param array $errors Tableau d'erreurs
+	 * @todo vérifier les droits
+	 */
+  function getServiceNewForm () {
+		$id = $this->getRequest('id', null);
+		$errors = $this->getRequest('errors', null);
+		$url = $this->getRequest('url', CopixConfig::get ('sso|sso_gael_url'));
+		$type = $this->getRequest('type', null);
+    
+    $criticErrors = array();
+    
+    if ($criticErrors)
+			return CopixActionGroup::process ('genericTools|Messages::getError', array ('message'=>implode('<br/>',$criticErrors), 'back'=>CopixUrl::get('sso||')));  
+
+    $tpl = & new CopixTpl ();
+		$tpl->assign ('TITLE_PAGE', CopixI18N::get ('sso.string.newsso'));
+
+		$tplForm = & new CopixTpl ();
+		$tplForm->assign ('id', $id);
+		$tplForm->assign ('message', $message);
+		$tplForm->assign ('url', $url);
+		$tplForm->assign ('type', $type);
+		$tplForm->assign ("errors", $errors);
+
+		$result = $tplForm->fetch('getServiceNewForm.tpl');
+		$tpl->assign ('MAIN', $result);
+			
+		return new CopixActionReturn (COPIX_AR_DISPLAY, $tpl);
+  
+  }
+
+   /**
+	 * Traitement du formulaire d'ajout d'un service SSO
+	 * 
+	 * @author Christophe Beyer <cbeyer@cap-tic.fr>
+	 * @since 2006/11/14
+	 * @param integer $sso Id de la collection SSO
+	 * @param string $url URL à interroger
+	 * @param string $type Type
+   * @todo vérifier les droits
+   */
+  function doServiceNewForm () {
+		$id = $this->getRequest('id', null);
+		$url = $this->getRequest('url', null);
+		$type = $this->getRequest('type', null);
+
+    $criticErrors = $errors = array();
+    
+    if ($criticErrors)
+			return CopixActionGroup::process ('genericTools|Messages::getError', array ('message'=>implode('<br/>',$criticErrors), 'back'=>CopixUrl::get('sso||')));  
+
+		if (!$url)	$errors[] = CopixI18N::get ('sso.error.newsso_url');
+		if (!$type)	$errors[] = CopixI18N::get ('sso.error.newsso_type');
+  
+//  print_r(htmlentities(CopixUrl::get('sso||doActivateService', array('id'=>$newSso->sso_auth_id))));
+    if (!$errors) { // Ok, on traite
+      
+      $dao = CopixDAOFactory::create("sso|sso_auth");
+			
+			$newSso = CopixDAOFactory::createRecord("sso_auth");
+			$newSso->sso_auth_id_sso = $id;
+			$newSso->sso_auth_login_local = $_SESSION['user']->bu['login'];
+			$newSso->sso_auth_type = $type;
+			$newSso->sso_auth_url = $url;
+			$newSso->sso_auth_date_crea = date("Y-m-d H:i:s");
+			$newSso->sso_auth_login_distant = '';
+			$newSso->sso_auth_secret_key = '';
+			$newSso->sso_auth_date_valid = '';
+			
+			$dao->insert ($newSso);
+			
+			if ($newSso->sso_auth_id !== NULL) {
+        $go = $url.'?mode=register'."&login_ico=".$_SESSION['user']->bu['login']."&url=".urlencode(CopixUrl::get().CopixUrl::get('sso||doActivateService', array('id'=>$newSso->sso_auth_id)));
+//        die ($go);
+        return new CopixActionReturn (COPIX_AR_REDIRECT, $go );
+			}
+
+    
+    }
+
+		return CopixActionGroup::process ('sso|sso::getServiceNewForm', array ('id'=>$id, 'errors'=>$errors, 'url'=>$url, 'type'=>$type));
+
+  
+  }
+
+   /**
+	 * Activation d'un service SSO. Adresse "cachée" utilisée par le site distant après vérification du compte à jumeler
+	 * 
+	 * @author Christophe Beyer <cbeyer@cap-tic.fr>
+	 * @since 2006/11/14
+	 * @param integer $id Id du SSO à activer
+	 * @param string $login_distant Login distant
+	 * @param string $key Clé utilisée pour communiquer entre les 2 sites
+   * @todo vérifier les droits et tester les erreurs. Afficher un message OK quand l'activation s'est bien passée ?
+   */
+  
+  function doActivateService () {
+  
+		$id = $this->getRequest('id', null);
+		$login_distant = $this->getRequest('login_distant', null);
+		$secret_key = $this->getRequest('cle', null);
+		$url_sso = $this->getRequest('url_sso', null);
+
+    $dao = CopixDAOFactory::create("sso|sso_auth");
+    $obj = $dao->get($id);
+
+    $criticErrors = $errors = array();
+    
+    if (!$obj)
+      $criticErrors[] = CopixI18N::get ('sso.error.nosso');
+    $mondroit = Kernel::getLevel( "MOD_SSO", $obj->sso_auth_id_sso);
+    if ($mondroit < PROFILE_CCV_ADMIN)
+      $criticErrors[] = CopixI18N::get ('sso.error.norights');
+
+    if ($criticErrors)
+			return CopixActionGroup::process ('genericTools|Messages::getError', array ('message'=>implode('<br/>',$criticErrors), 'back'=>CopixUrl::get('sso||')));  
+  
+    $obj->sso_auth_url = $url_sso;
+    $obj->sso_auth_login_distant = $login_distant;
+    $obj->sso_auth_secret_key = $secret_key;
+    $obj->sso_auth_date_valid = date("Y-m-d H:i:s");
+    $dao->update ($obj);
+    
+    return new CopixActionReturn (COPIX_AR_REDIRECT, CopixUrl::get('sso||'));
+  
+  }
+
+   /**
+	 * Suppression d'un service SSO.
+	 * 
+	 * @author Christophe Beyer <cbeyer@cap-tic.fr>
+	 * @since 2006/11/14
+	 * @param integer $id Id du SSO à supprimer
+   */
+  
+  function doDeleteService () {
+  
+		$id = $this->getRequest('id', null);
+
+    $dao = CopixDAOFactory::create("sso|sso_auth");
+    $obj = $dao->get($id);
+
+    $criticErrors = $errors = array();
+    
+    if (!$obj)
+      $criticErrors[] = CopixI18N::get ('sso.error.nosso');
+    $mondroit = Kernel::getLevel( "MOD_SSO", $obj->sso_auth_id_sso);
+    if ($mondroit < PROFILE_CCV_ADMIN)
+      $criticErrors[] = CopixI18N::get ('sso.error.norights');
+
+    if ($criticErrors)
+			return CopixActionGroup::process ('genericTools|Messages::getError', array ('message'=>implode('<br/>',$criticErrors), 'back'=>CopixUrl::get('sso||')));  
+  
+    $dao->delete ($id);
+
+    return new CopixActionReturn (COPIX_AR_REDIRECT, CopixUrl::get('sso||'));
+  
+  }
+
+}
+
+?>
