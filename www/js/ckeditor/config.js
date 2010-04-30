0b7eff79fc15fb1cf7aebe6ace1bcf39e1718eb4
Arnaud LEMAIRE <alemaire@cap-tic.fr>
2010-04-30 13:40:39 +0000

    Maj CK editor & suppression fichiers inutiles

--- www/js/ckeditor/config.js
+++ www/js/ckeditor/config.js
@@ -8,4 +8,19 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.toolbar = 'Iconito';
+
+    config.toolbar_Iconito =
+    [
+        ['Source','Preview'],
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-'],
+        ['Undo','Redo','-','Find','Replace','-','RemoveFormat'],
+        '/',
+        ['Styles'],
+        ['Bold','Italic','Strike'],
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        ['Link','Unlink','Anchor'],
+        ['Maximize','-','About']
+    ];
+
 };
