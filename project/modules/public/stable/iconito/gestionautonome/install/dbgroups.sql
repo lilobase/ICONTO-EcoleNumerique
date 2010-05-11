bb2e2fdd075e04bb5d6ae6c8a867b6003429a53a
Frédéric MADROLLE <fmadrolle@cap-tic.fr>
2010-05-11 07:08:30 +0000

    Première intégration de la gestion autonome.

--- project/modules/public/stable/iconito/gestionautonome/install/dbgroups.sql
+++
@@ -1,9 +0,0 @@
-#Insertion des groupes d'utilisateur génériques
-INSERT INTO `dbgroup` (`id_dbgroup`, `caption_dbgroup`, `description_dbgroup`, `superadmin_dbgroup`, `public_dbgroup`, `registered_dbgroup`) VALUES
-(3, 'cities_group_agent', NULL, 0, 0, 0),
-(4, 'city_agent', NULL, 0, 0, 0),
-(5, 'administration_staff', NULL, 0, 0, 0),
-(6, 'principal', NULL, 0, 0, 0),
-(7, 'teacher', NULL, 0, 0, 0),
-(8, 'cities_group_animator', NULL, 0, 0, 0),
-(9, 'schools_group_animator', NULL, 0, 0, 0);
