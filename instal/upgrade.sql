0f9e35e7e64caf5eca9ea5ad6e93d109a455cd7f
Arnaud LEMAIRE <alemaire@cap-tic.fr>
2010-07-09 15:05:57 +0000

    charte : SQL du module

--- instal/upgrade.sql
+++ instal/upgrade.sql
@@ -1,14 +1,45 @@
 -- Serveur: localhost
--- Généré le : Jeu 01 Juillet 2010 à 10:07
+-- Généré le : Ven 09 Juillet 2010 à 16:57
 -- Version du serveur: 5.1.44
 -- Version de PHP: 5.2.13
+-- --------------------------------------------------------
 
 --
--- Base de données: `ecolenumerique2010`
+-- Structure de la table `module_charte_chartes`
 --
+
+CREATE TABLE `module_charte_chartes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type` varchar(75) NOT NULL,
+  `file_url` varchar(500) NOT NULL,
+  `file_id` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_type` (`user_type`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
+--
+-- Structure de la table `module_charte_users_validation`
+--
+
+CREATE TABLE `module_charte_users_validation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `date` int(11) unsigned NOT NULL,
+  `charte_id` int(11) unsigned NOT NULL,
+  `user_type` varchar(75) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+
+-- Serveur: localhost
+-- Généré le : Jeu 01 Juillet 2010 à 10:07
+-- Version du serveur: 5.1.44
+-- Version de PHP: 5.2.13
+-- --------------------------------------------------------
 --
 -- Structure de la table `module_quiz_choices`
 --
