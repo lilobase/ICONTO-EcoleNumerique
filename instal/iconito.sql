1acb99d27d151af75dec97c6dc392ec0adadae9c
Frédéric MADROLLE <fmadrolle@cap-tic.fr>
2010-08-18 12:49:22 +0000

    Package ICONITO EcleNumerique 2010

--- instal/iconito.sql
+++ instal/iconito.sql
@@ -2336,3 +2336,146 @@ CREATE TABLE IF NOT EXISTS `module_rightmatrix` (
   KEY `user_type_in` (`user_type_in`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=54 ;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_mailext`
+--
+
+DROP TABLE IF EXISTS `module_mailext`;
+CREATE TABLE `module_mailext` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `protocol` varchar(255) NOT NULL,
+  `server` varchar(255) NOT NULL,
+  `port` int(2) unsigned NOT NULL,
+  `ssl` tinyint(2) unsigned NOT NULL,
+  `tls` tinyint(2) unsigned NOT NULL,
+  `login` varchar(150) NOT NULL,
+  `pass` varchar(150) NOT NULL,
+  `imap_path` varchar(150) DEFAULT NULL,
+  `name` varchar(150) NOT NULL,
+  `webmail_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_mailext`
+-- 
+
+DROP TABLE IF EXISTS `module_quiz_choices`;
+CREATE TABLE `module_quiz_choices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_question` int(11) unsigned NOT NULL,
+  `content` text,
+  `correct` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `order` int(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_quiz_groupes`
+--
+
+DROP TABLE IF EXISTS `module_quiz_groupes`;
+CREATE TABLE `module_quiz_groupes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_quiz_questions`
+--
+
+DROP TABLE IF EXISTS `module_quiz_questions`;
+CREATE TABLE `module_quiz_questions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(75) NOT NULL,
+  `id_quiz` int(11) unsigned NOT NULL,
+  `content` text,
+  `order` int(5) unsigned NOT NULL,
+  `opt_type` varchar(75) NOT NULL DEFAULT 'choice',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_quiz_quiz`
+--
+
+DROP TABLE IF EXISTS `module_quiz_quiz`;
+CREATE TABLE `module_quiz_quiz` (
+  `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
+  `id_owner` int(4) unsigned NOT NULL,
+  `date_start` int(5) unsigned NOT NULL DEFAULT '0',
+  `date_end` int(5) unsigned NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL,
+  `description` text,
+  `help` text,
+  `pic` varchar(255) DEFAULT NULL,
+  `opt_save` varchar(75) NOT NULL,
+  `opt_show_results` varchar(75) NOT NULL,
+  `lock` tinyint(4) NOT NULL DEFAULT '0',
+  `gr_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gr_id` (`gr_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_quiz_responses`
+--
+
+DROP TABLE IF EXISTS `module_quiz_responses`;
+CREATE TABLE `module_quiz_responses` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) unsigned NOT NULL,
+  `id_choice` int(11) unsigned NOT NULL,
+  `id_question` int(11) unsigned NOT NULL,
+  `date` int(11) unsigned NOT NULL,
+  `txt` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_charte_chartes`
+--
+
+DROP TABLE IF EXISTS `module_charte_chartes`
+CREATE TABLE `module_charte_chartes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type` varchar(75) NOT NULL,
+  `file_url` varchar(500) NOT NULL,
+  `file_id` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(2) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_type` (`user_type`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `module_charte_users_validation`
+--
+
+CREATE TABLE `module_charte_users_validation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `date` int(11) unsigned NOT NULL,
+  `charte_id` int(11) unsigned NOT NULL,
+  `user_type` varchar(75) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
