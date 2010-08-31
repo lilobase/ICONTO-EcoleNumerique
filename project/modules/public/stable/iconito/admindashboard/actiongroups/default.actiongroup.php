4a5f4befc6d702ffd60b90f73efb89242a520848
Arnaud LEMAIRE <alemaire@cap-tic.fr>
2010-08-31 12:54:27 +0000

    enic : utilitaire de créa de squelette module ajout squelette admindashboard

---
+++ project/modules/public/stable/iconito/admindashboard/actiongroups/default.actiongroup.php
@@ -0,0 +1,20 @@
+<?php
+
+    class ActionGroupDefault extends enicActionGroup {
+
+        public function __construct(){
+            parent::__construct();
+            $this->service =& $this->service('AdminDashboardService');
+        }
+
+        public function beforeAction (){
+		_currentUser()->assertCredential ('group:[current_user]');
+        }
+
+        public function processDefault(){
+            $ppo = new CopixPPO();
+
+            return _arPPO($ppo, 'default.tpl');
+        }
+
+    }
---
+++ project/modules/public/stable/iconito/admindashboard/classes/admindashboardservice.class.php
@@ -0,0 +1,5 @@
+<?php
+
+    class AdminDashboardService extends enicService{
+
+    }
