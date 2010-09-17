31b462edcc38d90f0d44898e696fe9a7a9dd8202
Stéphane HOLTZ <sholtz@cap-tic.fr>
2010-09-17 07:12:11 +0000

    ERGO - Ménage CSS

--- project/themes/default/styles.php
+++ project/themes/default/styles.php
@@ -9,11 +9,11 @@
 	Copyright (c) 2010 CAP-TIC <http://www.cap-tic.fr>
 */
 ?>
+<link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_deprecated.css"); ?>" type="text/css"/>
 <link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_layout.css"); ?>" type="text/css"/>
-<link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_zones.css"); ?>" type="text/css"/>
-<link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_colors.css"); ?>" type="text/css"/>
 <link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_typography.css"); ?>" type="text/css"/>
-<link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_debug.css"); ?>" type="text/css"/>
+<link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_buttons.css"); ?>" type="text/css"/>
+<link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/core_zones.css"); ?>" type="text/css"/>
 <link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/jquerycss/default/jquery-ui-1.8.2.custom.css"); ?>" type="text/css"/>
 <link rel="stylesheet" href="<?php echo CopixUrl::getResource ("js/fancybox/jquery.fancybox-1.3.1.css"); ?>" type="text/css"/>
 <link rel="stylesheet" href="<?php echo CopixUrl::getResource ("styles/module_kernel.css"); ?>" type="text/css"/>
