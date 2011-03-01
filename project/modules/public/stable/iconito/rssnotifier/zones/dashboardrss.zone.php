1230a8971f85c872660b2d4a81f6e9508650380a
Arnaud LEMAIRE <alemaire@cap-tic.fr>
2011-03-01 15:08:21 +0000

    suppression infos debug

--- project/modules/public/stable/iconito/rssnotifier/zones/dashboardrss.zone.php
+++ project/modules/public/stable/iconito/rssnotifier/zones/dashboardrss.zone.php
@@ -7,7 +7,6 @@ class ZoneDashboardRss extends enicZone {
     }
 
     function _createContent(&$toReturn) {
-        _dump($this->helpers->config('enable'));
         if ($this->helpers->config('enable') != 'false') {
             $this->addJs('js/iconito/module_rssnotifier.js');
             $this->js->addFileByTheme('js/jquery.tmpl.js');
