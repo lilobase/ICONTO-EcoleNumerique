2b3a705d7310e874e5818293ec5a07fdf697785f
Jérémy FOURNAISE <sebastien.cas@isics.fr>
2011-05-20 09:28:15 +0000

    Debug barre eleve

--- project/modules/public/stable/iconito/cahierdetextes/zones/menueleve.zone.php
+++ project/modules/public/stable/iconito/cahierdetextes/zones/menueleve.zone.php
@@ -13,24 +13,24 @@ class ZoneMenuEleve extends CopixZone {
 	  $ppo = new CopixPPO ();                               
     
     // Récupération des paramètres    
-    $ppo->nid = $this->getParam('nid');
-    $jour     = $this->getParam('date_jour');
-    $mois     = $this->getParam('date_mois');
-    $annee    = $this->getParam('date_annee');
+    $ppo->cahierId = $this->getParam('cahierId');
+    $ppo->jour     = $this->getParam('date_jour');
+    $ppo->mois     = $this->getParam('date_mois');
+    $ppo->annee    = $this->getParam('date_annee');
     
     // Timestamp de la date sélectionnée
-    $ppo->dateSelectionnee = mktime(0, 0, 0, $mois, $jour, $annee);
+    $ppo->dateSelectionnee = mktime(0, 0, 0, $ppo->mois, $ppo->jour, $ppo->annee);
     $numeroSemaine         = date('W', $ppo->dateSelectionnee);
     
     $service  = new DateService;
     
     // Récupération des timestamp des jours de la semaine
-		$ppo->lun = $service->numweekToDate($numeroSemaine, $annee, 1);
-		$ppo->mar = $service->numweekToDate($numeroSemaine, $annee, 2);
-		$ppo->mer = $service->numweekToDate($numeroSemaine, $annee, 3);
-		$ppo->jeu = $service->numweekToDate($numeroSemaine, $annee, 4);
-		$ppo->ven = $service->numweekToDate($numeroSemaine, $annee, 5);
-		$ppo->sam = $service->numweekToDate($numeroSemaine, $annee, 6);
+		$ppo->lun = $service->numweekToDate($numeroSemaine, $ppo->annee, 1);
+		$ppo->mar = $service->numweekToDate($numeroSemaine, $ppo->annee, 2);
+		$ppo->mer = $service->numweekToDate($numeroSemaine, $ppo->annee, 3);
+		$ppo->jeu = $service->numweekToDate($numeroSemaine, $ppo->annee, 4);
+		$ppo->ven = $service->numweekToDate($numeroSemaine, $ppo->annee, 5);
+		$ppo->sam = $service->numweekToDate($numeroSemaine, $ppo->annee, 6);
 
     $ppo->semainePrecedente = strtotime("-1 week", $ppo->lun);
     $ppo->semaineSuivante   = strtotime("+1 week", $ppo->lun);
