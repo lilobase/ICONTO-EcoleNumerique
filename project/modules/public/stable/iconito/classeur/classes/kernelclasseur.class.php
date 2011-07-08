84ae965601863aec80dcb82343ac1f9620fdb032
Jérémy FOURNAISE <sebastien.cas@isics.fr>
2011-07-08 07:49:50 +0000

    Methode create du KernelClasseur

--- project/modules/public/stable/iconito/classeur/classes/kernelclasseur.class.php
+++ project/modules/public/stable/iconito/classeur/classes/kernelclasseur.class.php
@@ -17,29 +17,29 @@ class KernelClasseur {
 		$return = null;
 		
 		_classInclude('classeur|classeurservice');
-		$dao = _dao("classeur|classeur");
-		$new = _record("classeur|classeur");
+		$dao = _dao('classeur|classeur');
+		$new = _record('classeur|classeur');
 		$new->titre = ($infos['title']) ? $infos['title'] : CopixI18N::get ('classeur|classeur.moduleDescription');
-		$new->cle = ClasseurService::createKey();
-		$new->date_creation = date("Y-m-d H:i:s");
+		$new->cle   = ClasseurService::createKey();
+		$new->date_creation = date('Y-m-d H:i:s');
 		$dao->insert ($new);
 
 		if (!is_null($new->id)) {
       
       $path2data = realpath('./static/classeur');
       $folder = $path2data.'/'.$new->id.'_'.$new->cle;
-      if ($mkdir = @mkdir ($folder, 0777)) {
+      if ($mkdir = mkdir($folder, 0777)) {
         
-        chmod ($folder, 0777);
+        chmod($folder, 0777);
         $return = $new->id;
       }
       
       if (!$return) {
         
-        // MalleService::deleteDir ($folder);
         $dao->delete ($new->id);
       }
 		}
+		
 		return $return;
 	}
 }
