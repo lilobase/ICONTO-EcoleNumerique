57fe57fe0f5f2df43c2978ae83172d756652237b
Jérémy FOURNAISE <sebastien.cas@isics.fr>
2011-07-11 13:58:52 +0000

    Debug arborescence vue popup

--- project/modules/public/stable/iconito/classeur/actiongroups/default.actiongroup.php
+++ project/modules/public/stable/iconito/classeur/actiongroups/default.actiongroup.php
@@ -1518,40 +1518,11 @@ class ActionGroupDefault extends enicActionGroup {
 		$ppo->dossiers = $dossierDAO->getEnfantsDirects($ppo->classeur->id, $ppo->dossierId)->fetchAll();
 		$ppo->fichiers = $fichierDAO->getParDossier($ppo->classeur->id, $ppo->dossierId)->fetchAll();
 		
-		// Récupération des classeurs accessibles à l'utilisateur
-    $classeurIds = array();
-    $nodes = Kernel::getMyNodes (_currentUser()->getExtra('type'), _currentUser()->getExtra('id'));
-    foreach ($nodes as $node) {
-      
-      $modules = Kernel::getModEnabled($node->type, $node->id, _currentUser()->getExtra('type'), _currentUser()->getExtra('id'));
-      foreach ($modules as $module) {
-        if ($module->module_type == 'MOD_CLASSEUR') {
-          // Identification du classeur personnel de l'utilisateur
-          if (strpos($module->node_type, 'USER_') !== false 
-            && ($module->node_type == _currentUser()->getExtra('type') && $module->node_id == _currentUser()->getExtra('id'))) {
-                
-                $ppo->classeurPersonnel = $module->module_id;
-                $classeurIds[] = $module->module_id;
-          }
-          elseif (!strpos($module->node_type, 'USER_')
-            && Kernel::getLevel('MOD_CLASSEUR', $module->module_id) > PROFILE_CCV_NONE) {
-            
-            $classeurIds[] = $module->module_id;
-          }
-        }
-      }
-    }
-    
-    // Dédoublonnage des classeurs (responsables avec plusieurs enfants)
-    $classeurIds = array_unique($classeurIds);
-    
-    $ppo->classeurs = array();
-    $classeurDAO = _ioDAO('classeur|classeur');
-    foreach ($classeurIds as $classeurId) {
-      
-      $classeur = $classeurDAO->get($classeurId);
-      $ppo->classeurs[] = $classeur;
-    }
+		// Ouverture du dossier courant dans l'arborescence
+		if ($ppo->dossierId != 0) {
+		  
+		  classeurService::openTree($ppo->classeur->id, $ppo->dossierId);
+		}
 		
 		CopixHTMLHeader::addJSLink (_resource('js/iconito/module_classeur.js')); 
  	  
