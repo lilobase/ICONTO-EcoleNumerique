c7d192ddf4a7c0ce0e894d7bc9254e9705ccb54a
Jérémy FOURNAISE <sebastien.cas@isics.fr>
2011-09-30 14:45:52 +0000

    #3765

--- project/modules/public/stable/iconito/cahierdetextes/classes/cahierdetextestravail2eleve.dao.php
+++ project/modules/public/stable/iconito/cahierdetextes/classes/cahierdetextestravail2eleve.dao.php
@@ -23,7 +23,7 @@ class DAOCahierDeTextesTravail2eleve {
 	  $toReturn = array();
 	  
 	  $sql = 'SELECT E.idEleve, E.nom, E.prenom1, CN.niveau_court, U.login_dbuser AS login, LI.bu_type, LI.bu_id, CL.nom as nom_classe' 
-      . ' FROM kernel_bu_eleve E, kernel_bu_eleve_affectation A, kernel_link_bu2user LI, dbuser U, kernel_bu_classe_niveau CN, kernel_bu_ecole_classe CL, module_cahierdetextes_travail2eleve AS T'
+      . ' FROM kernel_bu_eleve E, kernel_bu_eleve_affectation A, kernel_link_bu2user LI, dbuser U, kernel_bu_classe_niveau CN, kernel_bu_ecole_classe CL, module_cahierdetextes_travail2eleve AS T, module_cahierdetextes_travail as travail, module_cahierdetextes_domaine D'
 		  . ' WHERE E.idEleve = A.eleve'
 		  . ' AND E.idEleve = T.kernel_bu_eleve_idEleve'
 		  . ' AND A.classe = CL.id'
@@ -32,9 +32,12 @@ class DAOCahierDeTextesTravail2eleve {
 		  . ' AND U.id_dbuser = LI.user_id'
 		  . ' AND A.niveau=CN.id_n'
 		  . ' AND T.module_cahierdetextes_travail_id = :idTravail'
+		  . ' AND travail.id = T.module_cahierdetextes_travail_id'
+		  . ' AND travail.module_cahierdetextes_domaine_id = D.id'
+		  . ' AND D.kernel_bu_ecole_classe_id = CL.id'
 		  . ' GROUP BY E.idEleve'
 		  . ' ORDER BY E.nom, E.prenom1';
-	  
+		  
 	  return _doQuery ($sql, array(':idTravail' => $idTravail));
 	}
 }
