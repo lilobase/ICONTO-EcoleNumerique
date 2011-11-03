4d86f62c620ab111d4ba99ce035201fa60d7d914
Jérémy FOURNAISE <sebastien.cas@isics.fr>
2011-11-03 08:41:45 +0000

    Maj dao cahierdetextesmemo2eleve

--- project/modules/public/stable/iconito/cahierdetextes/classes/cahierdetextesmemo2eleve.dao.php
+++ project/modules/public/stable/iconito/cahierdetextes/classes/cahierdetextesmemo2eleve.dao.php
@@ -44,18 +44,21 @@ class DAOCahierDeTextesMemo2eleve {
 	  $toReturn = array();
 	  
 	  $sql = 'SELECT E.idEleve, E.nom, E.prenom1, CN.niveau_court, U.login_dbuser AS login, LI.bu_type, LI.bu_id, CL.nom as nom_classe, M.signe_le, M.commentaire' 
-      . ' FROM kernel_bu_eleve E, kernel_bu_eleve_affectation A, kernel_link_bu2user LI, dbuser U, kernel_bu_classe_niveau CN, kernel_bu_ecole_classe CL, module_cahierdetextes_memo2eleve AS M'
+      . ' FROM kernel_bu_eleve E, kernel_bu_eleve_affectation A, kernel_link_bu2user LI, dbuser U, kernel_bu_classe_niveau CN, kernel_bu_ecole_classe CL, module_cahierdetextes_memo2eleve AS M, kernel_bu_ecole_classe_niveau ECN'
 		  . ' WHERE E.idEleve = A.eleve'
-		  . ' AND E.idEleve = M.kernel_bu_eleve_idEleve'
+		  . ' AND A.eleve = M.kernel_bu_eleve_idEleve'
 		  . ' AND A.classe = CL.id'
-		  . ' AND LI.bu_type = "USER_ELE"'
-		  . ' AND LI.bu_id=E.idEleve'
-		  . ' AND U.id_dbuser = LI.user_id'
-		  . ' AND A.niveau=CN.id_n'
+		  . ' AND CL.id = ECN.classe'
+		  . ' AND A.niveau = CN.id_n'
 		  . ' AND A.current = 1'
+		  . ' AND ECN.niveau = CN.id_n'
+		  . ' AND A.eleve = LI.bu_id'
+		  . ' AND LI.user_id = U.id_dbuser'
+		  . ' AND LI.bu_type = "USER_ELE"'
 		  . ' AND M.module_cahierdetextes_memo_id = :idMemo'
 		  . ' GROUP BY E.idEleve'
 		  . ' ORDER BY E.nom, E.prenom1';
+		  
 	  
 	  return _doQuery ($sql, array(':idMemo' => $idMemo));
 	}
