5c735ab0f582ffabfdf7791f0dd05e2cc1d5ec2e
Frédéric MOSSMANN <fmossmann@cap-tic.fr>
2011-11-21 16:21:50 +0000

    [FIX] Classeur : Images taille web dans le diaporama "galleria"

--- project/modules/public/stable/iconito/classeur/actiongroups/default.actiongroup.php
+++ project/modules/public/stable/iconito/classeur/actiongroups/default.actiongroup.php
@@ -1551,7 +1551,7 @@ class ActionGroupDefault extends enicActionGroup {
         $image = new CopixPPO();
         $extension = strtolower(strrchr($fichier->fichier, '.'));
         $image->big = $fichier->id.'-'.$fichier->cle.$extension;
-        $image->web = $fichier->id.'-'.$fichier->cle.'_800'.$extension;
+        $image->web = $fichier->id.'-'.$fichier->cle.'_640'.$extension;
         $images[] = $image;
       }
     }
