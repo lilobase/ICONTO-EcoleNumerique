ee54d4841458b546d677f38baf0a3f38ad9a93dd
Philippe ROSER <proser@cap-tic.fr>
2012-07-17 17:31:05 +0200

    Merge branch 'feature-notifications' into EN2012

--- project/modules/public/stable/iconito/cahierdetextes/classes/kernelcahierdetextes.class.php
+++ project/modules/public/stable/iconito/cahierdetextes/classes/kernelcahierdetextes.class.php
@@ -84,5 +84,55 @@ class KernelCahierDeTextes
         return $res;
     }
 
+	function getNotifications(&$module, &$lastvisit) {
+		
+		$module->notification_number = 0;
+		$module->notification_message = "";
+		
+		// echo "<pre>"; print_r($module); die();
+		
+		// Le module CahierDetextes ne fonctionne actuellement que pour les classes.
+		if( $module->node_type == 'BU_CLASSE' ) $id_eleve = _currentUser()->getExtra('id');
+		elseif( $module->node_type == 'USER_ELE' ) $id_eleve = $module->node_id;
+		else return true;
+		
+		// Les notifications sont pour les élèves uniquement.
+		if( _currentUser()->getExtra("type") == 'USER_ENS' ) return true;
+		
+		$news_travaux = _doQuery('SELECT * 
+			FROM module_cahierdetextes_travail2eleve T2E
+			JOIN module_cahierdetextes_travail T
+				ON T2E.module_cahierdetextes_travail_id=T.id
+			WHERE T2E.kernel_bu_eleve_idEleve=:id_eleve
+				AND T.supprime=0
+				AND T.timestamp>:lastvisit
+				AND T.a_faire=1
+			', array(
+				':id_eleve'=>_currentUser()->getExtra('id'),
+				':lastvisit'=>$lastvisit->date
+			));
+			
+		$news_memos = _doQuery('SELECT * 
+			FROM module_cahierdetextes_memo2eleve M2E
+			JOIN module_cahierdetextes_memo M
+				ON M2E.module_cahierdetextes_memo_id=M.id
+			WHERE M2E.kernel_bu_eleve_idEleve=:id_eleve
+				AND M.supprime=0
+				AND M.timestamp>:lastvisit
+			', array(
+				':id_eleve'=>_currentUser()->getExtra('id'),
+				':lastvisit'=>$lastvisit->date
+			));
+			
+		$module->notification_number = count($news_travaux)+count($news_memos);
+		$module->notification_message = '';
+		if(count($news_travaux)) $module->notification_message.= (count($news_travaux)>1?CopixI18N::get('cahierdetextes|cahierdetextes.notification.travaux_n', array(count($news_travaux))):CopixI18N::get('cahierdetextes|cahierdetextes.notification.travaux_1', array(count($news_travaux))));
+		if(count($news_travaux)&&count($news_memos)) $module->notification_message.=' et ';
+		if(count($news_memos  )) $module->notification_message.= (count($news_memos  )>1?CopixI18N::get('cahierdetextes|cahierdetextes.notification.memos_n'  , array(count($news_memos  ))):CopixI18N::get('cahierdetextes|cahierdetextes.notification.memos_1'  , array(count($news_memos  ))));
+		$module->notification_message = ucfirst($module->notification_message);
+		return true;
+	}
+
+
 }
 
