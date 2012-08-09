b60d393871ef6d1504d5a2a63c14198a2c3049af
Frédéric MOSSMANN <fmossmann@cap-tic.fr>
2012-08-09 17:50:14 +0200

    [FIX] Gestion Autonome : Supprime le cache de la matrice des droits à l'affectation d'un enseignant dans une classe.

--- project/modules/public/stable/iconito/gestionautonome/actiongroups/default.actiongroup.php
+++ project/modules/public/stable/iconito/gestionautonome/actiongroups/default.actiongroup.php
@@ -4676,6 +4676,11 @@ class ActionGroupDefault extends enicActionGroup {
           // Mise à jour de l'affectation enseignant à l'école pour passage en directeur
           $personEntityDAO->updateRole ($personId, $nodeInfos['ALL']->eco_numero, 'ECOLE', $ppo->role);
         }
+
+        $user_infos = Kernel::getUserInfo('USER_ENS', $personId);
+        if($user_infos) {
+          @unlink(COPIX_TEMP_PATH.'cache/enic/matrix'.($user_infos['user_id']).'.cache');
+        }
   	  }
 	  }
 	  
