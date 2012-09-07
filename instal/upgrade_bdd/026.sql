949f5605bc165e812338617b2d841ae877926dc8
Frédéric MADROLLE <fmadrolle@cap-tic.fr>
2012-09-07 17:54:29 +0200

    [FIX] dashboard : suppression de doublons dans la table

--- instal/upgrade_bdd/026.sql
+++ instal/upgrade_bdd/026.sql
@@ -1,3 +1,5 @@
+DELETE FROM module_admindash WHERE content IS NULL AND picture IS NULL;
+
 ALTER TABLE  `module_admindash` ADD  `social_stream` VARCHAR( 255 ) NULL;
 ALTER TABLE `module_admindash` DROP `id`;
 ALTER TABLE module_admindash DROP INDEX id_zone;
