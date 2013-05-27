66b0dab16ad5fa976b396982d25a7658505fb4ca
Julien POTTIER <julien.pottier@isics.fr>
2013-05-27 17:18:03 +0200

    [NEW] Ajout des premiers fichiers permettant de gérer la configuration d'une classe

---
+++ project/modules/public/stable/iconito/classe/actiongroups/default.actiongroup.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package    Iconito
+ * @subpackage Classe
+ * @author     Julien POTTIER <julien.pottier@isics.fr>
+ */
+
+class ActionGroupDefault extends CopixActionGroup
+{
+  public function processConfigure()
+  {
+
+  }
+}
