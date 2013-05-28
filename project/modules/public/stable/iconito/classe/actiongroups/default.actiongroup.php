372fcfdb44f656361b515b33d3c29142796b7f68
Julien POTTIER <julien.pottier@isics.fr>
2013-05-28 14:45:11 +0200

    [NEW] Ajout du formulaire de modification des préférences d'une classe. Ajout d'une méthode permettant de savoir si un module est activé pour une ressource. [UPDATE] Coding style sur le fichier kernel.class.php

--- project/modules/public/stable/iconito/classe/actiongroups/default.actiongroup.php
+++ project/modules/public/stable/iconito/classe/actiongroups/default.actiongroup.php
@@ -5,11 +5,30 @@
  * @subpackage Classe
  * @author     Julien POTTIER <julien.pottier@isics.fr>
  */
-
 class ActionGroupDefault extends CopixActionGroup
 {
-  public function processConfigure()
-  {
+    public function processConfigure()
+    {
+        _classInclude('kernel/Kernel');
+        $ppo    = new CopixPPO ();
+        $kernel = new Kernel();
+        $classe = CopixSession::get('myNode');
+        // Si le type de l'objet manipulé n'est pas une classe, on sort en erreur
+        if ($classe['type'] !== 'BU_CLASSE') {
+            return CopixActionGroup::process('generictools|Messages::getError', array(
+                'message' => CopixI18N::get('kernel|kernel.error.errorOccurred'),
+                'back'    => CopixUrl::get('')
+            ));
+        }
+        if (CopixRequest::isMethod('post')) {
+            _classInclude('classe|classe_parameters');
+            $classeParameters = new ClasseParameters($classe);
+            $classeParameters->process(array(
+                'minimail' => _request('minimail', false)
+            ));
+        }
+        $ppo->has_minimail_enabled = $kernel->hasRegisteredModule('MOD_MINIMAIL', 0, 'BU_CLASSE', $classe['id']);
 
-  }
+        return _arPPO($ppo, 'configure.tpl');
+    }
 }
