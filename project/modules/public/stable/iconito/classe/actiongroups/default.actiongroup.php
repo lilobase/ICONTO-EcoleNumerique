e99e3ba33ae20d1dd642a74aa15c30e72e4fd075
Julien POTTIER <julien.pottier@isics.fr>
2013-05-28 15:32:19 +0200

    [UPDATE] Modification du nom de la classe

--- project/modules/public/stable/iconito/classe/actiongroups/default.actiongroup.php
+++ project/modules/public/stable/iconito/classe/actiongroups/default.actiongroup.php
@@ -9,19 +9,31 @@ class ActionGroupDefault extends CopixActionGroup
 {
     public function processConfigure()
     {
-        _classInclude('kernel/Kernel');
         $ppo    = new CopixPPO ();
+
+        _classInclude('kernel/Kernel');
         $kernel = new Kernel();
+
         $classe = CopixSession::get('myNode');
         // Si le type de l'objet manipulé n'est pas une classe, on sort en erreur
         if ($classe['type'] !== 'BU_CLASSE') {
             return CopixActionGroup::process('generictools|Messages::getError', array(
                 'message' => CopixI18N::get('kernel|kernel.error.errorOccurred'),
-                'back'    => CopixUrl::get('')
+                'back'    => CopixUrl::get('||')
             ));
         }
+
+        // Seul l'enseignant de la classe peut accéder à ce paramétrage
+        if (!$kernel->isEnseignantOfClasse($classe['id']))
+        {
+            return CopixActionGroup::process('generictools|Messages::getError', array(
+                'message' => CopixI18N::get('kernel|kernel.error.errorOccurred'),
+                'back'    => CopixUrl::get('||')
+            ));
+        }
+
         if (CopixRequest::isMethod('post')) {
-            _classInclude('classe|classe_parameters');
+            _classInclude('classe|ClasseParameters');
             $classeParameters = new ClasseParameters($classe);
             $classeParameters->process(array(
                 'minimail' => _request('minimail', false)
