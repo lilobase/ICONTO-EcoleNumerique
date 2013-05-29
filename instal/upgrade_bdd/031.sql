8a75bf75b81302ef186008d33b7a657a0b15154f
Jérémy Hubert <jeremy.hubert@infogroom.fr>
2013-05-29 11:50:17 +0200

    [UPDATE] Ajout d'un patch pour l'ajout du module minimail a toutes les classes

---
+++ instal/upgrade_bdd/031.sql
@@ -0,0 +1,8 @@
+-- Autorisation par défaut des minimails pour toutes les classes
+INSERT INTO `kernel_mod_enabled`
+    SELECT 'BU_CLASSE', id, 'MOD_MINIMAIL', 0
+    FROM `kernel_bu_ecole_classe`
+    WHERE annee_scol IN (
+        SELECT id_as AS annee from `kernel_bu_annee_scolaire` WHERE current = 1
+    )
+;
