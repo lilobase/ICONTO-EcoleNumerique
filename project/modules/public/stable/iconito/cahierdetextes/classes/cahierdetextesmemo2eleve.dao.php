3f5c14dec869360cdb07c060520bfbf57a85098b
Jérémy Hubert <jeremy.hubert@infogroom.fr>
2013-06-05 10:49:43 +0200

    [UPDATE] Mémo : Cochage automatique de la classe en cours d'édition

--- project/modules/public/stable/iconito/cahierdetextes/classes/cahierdetextesmemo2eleve.dao.php
+++ project/modules/public/stable/iconito/cahierdetextes/classes/cahierdetextesmemo2eleve.dao.php
@@ -33,33 +33,12 @@ class DAOCahierDeTextesMemo2eleve
     }
 
     /**
-     * Retourne les identifiants des classes concernées par un memo
+     * Retourne le suivi des élèves concernés par un memo
      *
      * @param int $idMemo
      *
      * @return array
      */
-    public function findClassesParMemo ($idMemo)
-    {
-        $toReturn = array();
-
-//        $sql = 'SELECT kernel_bu_eleve_idEleve as idEleve FROM module_cahierdetextes_memo2eleve WHERE module_cahierdetextes_memo_id = :idMemo GROUP BY idEleve';
-//
-//        $resultats = _doQuery ($sql, array(':idMemo' => $idMemo));
-//        foreach($resultats as $resultat) {
-//            $toReturn[] = $resultat->idEleve;
-//        }
-
-        return empty($toReturn) ? null : $toReturn;
-    }
-
-    /**
-   * Retourne le suivi des élèves concernés par un memo
-   *
-   * @param int $idMemo
-   *
-   * @return array
-   */
     public function findSuiviElevesParMemo ($idMemo)
     {
       $toReturn = array();
