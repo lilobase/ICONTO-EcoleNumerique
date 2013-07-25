4b07027408be0aadd13872b70746e1d005afa277
Arnaud LEMAIRE <alemaire@cap-tic.fr>
2013-07-25 21:38:45 +0200

    duplicate entry in data.sql

--- instal/data.sql
+++ instal/data.sql
@@ -327,11 +327,11 @@ INSERT INTO `module_rightmatrix` (`id`, `user_type_in`, `user_type_out`, `right`
 (53, 'USER_ATI', 'USER_ENS', 'VOIR', 'ROOT'),
 (54, 'USER_ATI', 'USER_ENS', 'COMM', 'ROOT'),
 (55, 'USER_ATI', 'USER_ATI', 'VOIR', 'ROOT'),
-(55, 'USER_ATI', 'USER_ATI', 'COMM', 'ROOT'),
-(56, 'USER_ATI', 'USER_EXT', 'VOIR', 'ROOT'),
-(57, 'USER_ATI', 'USER_EXT', 'COMM', 'ROOT'),
-(58, 'USER_EXT', 'USER_EXT', 'VOIR', 'ROOT'),
-(59, 'USER_EXT', 'USER_EXT', 'COMM', 'ROOT');
+(56, 'USER_ATI', 'USER_ATI', 'COMM', 'ROOT'),
+(57, 'USER_ATI', 'USER_EXT', 'VOIR', 'ROOT'),
+(58, 'USER_ATI', 'USER_EXT', 'COMM', 'ROOT'),
+(59, 'USER_EXT', 'USER_EXT', 'VOIR', 'ROOT'),
+(60, 'USER_EXT', 'USER_EXT', 'COMM', 'ROOT');
 
 --
 -- Contenu de la table `module_teleprocedure_statu`
